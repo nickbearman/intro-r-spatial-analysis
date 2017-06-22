7b920365f745fba77e695f1461f77e0c8be0cf7b
Nick Bearman <nick@clearmapping.co.uk>
2017-06-22 12:32:47 +0100

    Changes as a result of Neighbourhood Statistics website no longer being avaliable

--- script-single-map.R
+++ script-single-map.R
@@ -4,18 +4,20 @@
   library(classInt)
   library(RColorBrewer)
 #download csv file
-  download.file("http://www.nickbearman.me.uk/data/r/KS102EW_2596_2011SOA_NW.csv","KS102EW_2596_2011SOA_NW.csv")
+  download.file("http://www.nickbearman.me.uk/data/r/nomis-2011-age-data.zip","nomis-2011-age-data.zip")
+#unzip csv file
+  unzip("nomis-2011-age-data.zip")
 #read in csv file to pop2011
-  pop2011 <- read.csv("KS102EW_2596_2011SOA_NW.csv", header = TRUE, skip = 5)
+  pop2011 <- read.csv("bulk.csv", header = TRUE)
 #create a new variable which contains the new variable names
-  newcolnames <- c("AllUsualResidentsc","Age00to04c","Age00to04pc","Age05to07c","Age05to07pc",
-                   "Age08to09c","Age08to09pc","Age10to14c","Age10to14pc","Age15c","Age15pc","Age16to17c",
-                   "Age16to17pc","Age18to19c","Age18to19pc","Age20to24c","Age20to24pc","Age25to29c",
-                   "Age25to29pc","Age30to44c","Age30to44pc","Age45to59c","Age45to59pc","Age60to64c",
-                   "Age60to64pc","Age65to74c","Age65to74pc","Age75to84c","Age75to84pc","Age85to89c",
-                   "Age85to89pc","Age90andOverc","Age90andOverpc","MeanAge","MedianAge")
+  newcolnames <- c("AllUsualResidentsc","Age00to04","Age05to07",
+                   "Age08to09","Age10to14","Age15","Age16to17",
+                   "Age18to19","Age20to24","Age25to29",
+                   "Age30to44","Age45to59","Age60to64",
+                   "Age65to74","Age75to84","Age85to89",
+                   "Age90andOver","MeanAge","MedianAge")
 #apply these to pop2011 data frame
-  colnames(pop2011)[11:45] <- newcolnames
+  colnames(pop2011)[5:23] <- newcolnames
 #download shapefile
   download.file("http://www.nickbearman.me.uk/data/r/england_lsoa_2011.zip","england_lsoa_2011.zip")
 #unzip shapefile
@@ -23,9 +25,9 @@
 #read in shapefile
   LSOA <- readOGR(".", "england_lsoa_2011")
 #join attribute data to LSOA
-  LSOA@data <- merge(pop2011,LSOA@data,by.x="LSOA_CODE",by.y="code")
+  LSOA@data <- merge(pop2011,LSOA@data,by.x="geography.code",by.y="code")
 #select variable
-  var <- LSOA@data[,"Age00to04pc"]
+  var <- LSOA@data[,"Age00to04"]
 #set colours & breaks
   breaks <- classIntervals(var, n = 6, style = "fisher")
   my_colours <- brewer.pal(6, "Greens")
